6beef42f27aad3704279680c90a5c749aaacfcd5
Nollee <48550469+Nollee@users.noreply.github.com>
2020-05-07 12:50:48 +0200

    Add files via upload

---
+++ precache-manifest.a128fac6c7ccb2ac39e7e39cedcee1d7.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aae70fb9e6971a487e4d",
+    "url": "/css/about.74900e2c.css"
+  },
+  {
+    "revision": "cdd37080bba20d717162",
+    "url": "/css/app.1069f78f.css"
+  },
+  {
+    "revision": "0034456219a259fc671960bb991743dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "aae70fb9e6971a487e4d",
+    "url": "/js/about.9c7ee6ca.js"
+  },
+  {
+    "revision": "cdd37080bba20d717162",
+    "url": "/js/app.44afdb6f.js"
+  },
+  {
+    "revision": "4c7ea10003f11c20abca",
+    "url": "/js/chunk-vendors.14e52255.js"
+  },
+  {
+    "revision": "5681e88a038a6cacf69911e8d28eafae",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
